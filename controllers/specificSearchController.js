9d6cf5750f18051f72449251e5c71b08db01a988
Ernesto Hernandez <h_ernesto@ymail.com>
2018-07-10 21:22:33 -0700

    trying environment variable without calling process

--- controllers/specificSearchController.js
+++ controllers/specificSearchController.js
@@ -1,10 +1,12 @@
 const request = require("request");
 module.exports = {
     specificSearch: (req, res) => {
+        console.log(process.env.NYT_API_KEY);
+        console.log(NYT_API_KEY);
         request.get({
             url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
             qs: {
-                'api-key': process.env.NYT_API_KEY  || NYT_API_KEY,
+                'api-key': NYT_API_KEY,
                 'q': req.query.topic,
                 // "begin_date": req.query.startDate,
                 // "end_date": req.query.endDate
